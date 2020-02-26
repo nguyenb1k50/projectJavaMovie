104f6ac336466374523db4b949c6ffb1ae136f3b
zachpro1935 <zachpro1935@gmail.com>
2020-02-26 23:47:00 +0700

    feature visa payment 26/02/2020

---
+++ src/main/java/com/myclass/controller/PaymentController.java
@@ -0,0 +1,41 @@
+package com.myclass.controller;
+
+import com.myclass.service.StripeClient;
+import com.stripe.model.Charge;
+import com.stripe.model.Customer;
+
+import net.bytebuddy.asm.Advice.This;
+
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/payment")
+public class PaymentController {
+	private StripeClient stripeClient;
+
+	@Autowired
+	PaymentController(StripeClient stripeClient) {
+		this.stripeClient = stripeClient;
+	}
+
+	@PostMapping("/charge")
+	public Object chargeCard(HttpServletRequest request) throws Exception {
+		String token = request.getHeader("token");
+		Double amount = Double.parseDouble(request.getHeader("amount"));
+		String cusId = request.getHeader("cusId");
+		Boolean isSaveCard = Integer.parseInt(request.getHeader("save")) == 1 ? true : false;
+		//case 1: already save card
+		if(cusId != null && !cusId.isEmpty()) {
+			return this.stripeClient.chargeCustomerCard(cusId, amount);
+		}else if (isSaveCard) { //case 2: first time or new card
+			// create new stripe customer then save to database so next time we just send cusId
+			Customer newCus = this.stripeClient.createCustomer(token, "zachpro1935@gmail.com");
+			return this.stripeClient.chargeCustomerCard(newCus.getId(), amount);
+		} else //case 3: charge 1 time
+			return this.stripeClient.chargeNewCard(token, amount);
+	}
+}
