7af1af038049856c33b4a9dc8269808fe6a2ac99
zachpro1935 <zachpro1935@gmail.com>
2020-04-07 23:54:17 +0700

    zach commit : add constraint unique for userDTO

---
+++ src/main/java/com/myclass/validate/UniqueUserNameConstraint.java
@@ -0,0 +1,19 @@
+package com.myclass.validate;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+
+@Constraint(validatedBy = UserUniqueValidator.class)
+@Target( { ElementType.METHOD, ElementType.FIELD })
+@Retention(RetentionPolicy.RUNTIME)
+public @interface UniqueUserNameConstraint {
+	String message() default "email trung nhe!";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
