89ff0a97143f7589d42f03a0539b725b79050914
zachpro1935 <zachpro1935@gmail.com>
2020-04-19 00:03:23 +0700

    zach add calendar feature

---
+++ src/main/java/com/myclass/controller/CalendarController.java
@@ -0,0 +1,89 @@
+package com.myclass.controller;
+
+import java.sql.Time;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.myclass.entity.Calendar;
+import com.myclass.repository.CalendarRepository;
+
+@RestController
+@RequestMapping("api/calendar")
+public class CalendarController {
+
+	@Autowired
+	CalendarRepository calendarRepository;
+	
+	@GetMapping("")
+	public Object get() {
+		List<Calendar> calenders = calendarRepository.getAllCalendar();
+		return new ResponseEntity<List<Calendar>>(calenders, HttpStatus.OK);
+	}
+	
+	@PostMapping()
+	public Object post(@RequestBody Calendar calender) {
+		Calendar cal = calendarRepository.checkExistCal(calender.getOpenDate(), calender.getMovieId(), calender.getCinemarId());
+		if(cal != null) {
+			Iterator<Time> listTime = cal.getTime().iterator();
+			while (listTime.hasNext()) {
+				int movieDuration = (cal.getMovie().getDuration()+15) * 60000;
+				Long from = listTime.next().getTime();				
+				Long end = from +movieDuration;
+				Iterator<Time> listNewTime = calender.getTime().iterator();
+				while (listNewTime.hasNext()) {
+					Long newTime = listNewTime.next().getTime();
+					//check new time
+					if(newTime >= from && newTime <= end || newTime+movieDuration >= from && newTime+movieDuration <= end) {
+						return new ResponseEntity<String>("trung gio chieu!!!", HttpStatus.BAD_REQUEST);
+					}
+				}
+			}
+			ArrayList<Time> t = cal.getTime();
+			t.addAll(calender.getTime());
+			cal.setTime(t);
+			Calendar c = calendarRepository.save(cal);
+			return new ResponseEntity<Calendar>(c, HttpStatus.CREATED);
+		}else {
+			calender.setId(UUID.randomUUID().toString());
+			Calendar c = calendarRepository.save(calender);
+			return new ResponseEntity<Calendar>(c, HttpStatus.CREATED);
+		}
+		
+	}
+	
+//	@PutMapping("/{id}")
+//	public Object post(@PathVariable String id, @RequestBody Catagory catagory) {
+//		
+//		if(catagoryRepository.existsById(id)) {
+//			catagory.setId(id);
+//			Catagory entity = catagoryRepository.save(catagory);
+//			return new ResponseEntity<Catagory>(entity, HttpStatus.OK);	
+//		}
+//		return new ResponseEntity<String>("Id không tồn tại!", HttpStatus.BAD_REQUEST);
+//	}
+//	
+//	
+//	@DeleteMapping("/{id}")
+//	public Object delete(@PathVariable String id) {
+//		
+//		if(catagoryRepository.existsById(id)) {
+//			catagoryRepository.deleteById(id);
+//			return new ResponseEntity<HttpStatus>(HttpStatus.NO_CONTENT);	
+//		}
+//		return new ResponseEntity<String>("Id không tồn tại!", HttpStatus.BAD_REQUEST);
+//	}
+}
--- src/main/java/com/myclass/entity/Calendar.java
+++ src/main/java/com/myclass/entity/Calendar.java
@@ -2,9 +2,10 @@ package com.myclass.entity;
 
 import java.sql.Time;
 import java.util.ArrayList;
-import java.util.Date;
+import java.sql.Date;
 import java.util.List;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.ForeignKey;
@@ -19,66 +20,93 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 @Entity
 @Table(name = "calendars")
 public class Calendar {
-@Id
-private String id;
-private Date openDate;
-private ArrayList<Time> time;
+	@Id
+	private String id;
+	private Date openDate;
+	private ArrayList<Time> time;
+	@Column(name = "movie_id")
+	private String movieId;
+	@ManyToOne
+	@JoinColumn(name = "movie_id", foreignKey = @ForeignKey(name = "fk_movie_calendar"), insertable = false, updatable = false)
+	@JsonIgnore
+	private Movie movie;
+	public String getMovieId() {
+		return movieId;
+	}
 
+	public void setMovieId(String movieId) {
+		this.movieId = movieId;
+	}
 
-@ManyToOne
-@JoinColumn(name = "movie_id", foreignKey = @ForeignKey(name="fk_movie_calendar"),
-insertable = false, updatable = false)
+	public String getCinemarId() {
+		return cinemarId;
+	}
 
-@JsonIgnore
-private Movie movie;
+	public void setCinemarId(String cinemarId) {
+		this.cinemarId = cinemarId;
+	}
 
-@OneToMany(mappedBy= "calendar", fetch = FetchType.LAZY)
-private List<Booking> bookings;
+	@Column(name = "cinemar_id")
+	private String cinemarId;
+	@ManyToOne
+	@JoinColumn(name = "cinemar_id", foreignKey = @ForeignKey(name="fk_cinemar_calendar"),
+	insertable = false, updatable = false)
+	@JsonIgnore
+	private Cinemar cinemar;
+	
+	@OneToMany(mappedBy = "calendar", fetch = FetchType.LAZY)
+	private List<Booking> bookings;
 
-public String getId() {
-	return id;
-}
+	public String getId() {
+		return id;
+	}
 
-public void setId(String id) {
-	this.id = id;
-}
+	public void setId(String id) {
+		this.id = id;
+	}
 
+	public Date getOpenDate() {
+		return openDate;
+	}
 
-public Date getOpenDate() {
-	return openDate;
-}
+	public void setOpenDate(Date openDate) {
+		this.openDate = openDate;
+	}
 
-public void setOpenDate(Date openDate) {
-	this.openDate = openDate;
-}
+	public Movie getMovie() {
+		return movie;
+	}
 
-public Movie getMovie() {
-	return movie;
-}
+	public void setMovie(Movie movie) {
+		this.movie = movie;
+	}
 
-public void setMovie(Movie movie) {
-	this.movie = movie;
-}
+	public Calendar() {
+		super();
+	}
 
+	public ArrayList<Time> getTime() {
+		return time;
+	}
 
-public Calendar() {
-	super();
-}
+	public void setTime(ArrayList<Time> time) {
+		this.time = time;
+	}
 
-public ArrayList<Time> getTime() {
-	return time;
-}
+	public Cinemar getCinemar() {
+		return cinemar;
+	}
 
-public void setTime(ArrayList<Time> time) {
-	this.time = time;
-}
+	public void setCinemar(Cinemar cinemar) {
+		this.cinemar = cinemar;
+	}
 
-public Calendar(String id, Date openDate, ArrayList<Time> time, Movie movie) {
-	super();
-	this.id = id;
-	this.openDate = openDate;
-	this.time = time;
-	this.movie = movie;
-}
+	public Calendar(String id, Date openDate, ArrayList<Time> time, Movie movie) {
+		super();
+		this.id = id;
+		this.openDate = openDate;
+		this.time = time;
+		this.movie = movie;
+	}
 
 }
