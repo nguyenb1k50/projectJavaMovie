59aff884d87c947e6bda7d20f9395a11b88a5616
zachpro1935 <zachpro1935@gmail.com>
2020-05-09 17:36:05 +0700

    zach update user

--- src/main/java/com/myclass/config/StaticCons.java
+++ src/main/java/com/myclass/config/StaticCons.java
@@ -29,6 +29,9 @@ public class StaticCons {
 			,"C1","C2","C3","C4","C5","C6","C7","C8","C9","C10"
 			,"D1","D2","D3","D4","D5","D6","D7","D8","D9","D10"
 			,"E1","E2","E3","E4","E5","E6","E7","E8","E9","E10"
+			,"F1","F2","F3","F4","F5","F6","F7","F8","F9","F10"
+			,"G1","G2","G3","G4","G5","G6","G7","G8","G9","G10"
+			,"H1","H2","H3","H4","H5","H6","H7","H8","H9","H10"
 			));
 	private static String UPLOAD_DIR = "src/main/resources/static/upload";
 	@Autowired
--- src/main/java/com/myclass/controller/UserController.java
+++ src/main/java/com/myclass/controller/UserController.java
@@ -12,6 +12,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -19,7 +20,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.myclass.config.StaticCons;
 import com.myclass.dto.UserDto;
+import com.myclass.entity.Movie;
 import com.myclass.entity.UserDTO;
 import com.myclass.repository.UserRepository;
 import com.myclass.validate.Authorized;
@@ -43,7 +46,7 @@ public class UserController {
 		return new ResponseEntity<List<UserDto>>(users, HttpStatus.OK);
 	}
 	
-	@Authorized(role = "user")
+	//@Authorized(role = "user")
 	@GetMapping("/current")
 	public Object getCurentUserInfo() {
 		String currentUserName;
@@ -79,8 +82,11 @@ public class UserController {
 	public Object post(@PathVariable String id, @RequestBody UserDTO user) {
 		
 		if(userRepository.existsById(id)) {
-			user.setId(id);
-			UserDTO entity = userRepository.save(user);
+			UserDTO u = userRepository.findById(id).get();
+			//for update partial property
+			StaticCons.copyNonNullProperties(user, u);
+			
+			UserDTO entity = userRepository.save(u);
 			return new ResponseEntity<UserDTO>(entity, HttpStatus.OK);	
 		}
 		return new ResponseEntity<String>("Id không tồn tại!", HttpStatus.BAD_REQUEST);
@@ -94,5 +100,14 @@ public class UserController {
 			return new ResponseEntity<HttpStatus>(HttpStatus.NO_CONTENT);	
 		}
 		return new ResponseEntity<String>("Id không tồn tại!", HttpStatus.BAD_REQUEST);
+	}
+	
+	@GetMapping("/updateStatus/{id}")
+	public Object updateUserStatus(@PathVariable String id) {
+		UserDTO u = userRepository.findById(id).get();
+		boolean flag = u.getActive() == true ? false : true ;
+		u.setActive(flag);
+		userRepository.save(u);
+		return new ResponseEntity<UserDTO>(u, HttpStatus.OK);
 	}
 }
